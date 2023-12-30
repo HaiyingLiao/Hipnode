e0f084c4e4eb596276e05290efb80a933d02c349
Hannah-Liao <hanna.liao@outlook.com>
2023-12-30 10:35:10 +0000

    add interviews validations

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,9 +1,28 @@
 import * as z from 'zod';
 
-export const CreatePostSchema = z.object({
-  title: z.string().min(5).max(130),
-  post: z.string().min(100),
-  group: z.string(),
-  createType: z.string(),
-  tags: z.array(z.string().min(1).max(15)).min(1).max(5),
-});
+export const PostSchema = z
+  .object({
+    title: z.string().min(5).max(130),
+    post: z.string().min(100),
+    postImage: z.string(),
+    group: z.string(),
+    createType: z.string(),
+    tags: z.array(z.string().min(1).max(15)).min(1).max(5),
+  })
+  .partial();
+
+export const CreatePostSchema = PostSchema.extend({
+  postImageKey: z.string(),
+}).required();
+
+export const InterviewsSchema = PostSchema.extend({
+  revenue: z.number(),
+  updates: z.number(),
+  website: z.string(),
+  authorId: z.string(),
+  category: z.string(),
+}).required();
+
+export type CreatePostType = z.infer<typeof CreatePostSchema>;
+export type UpdatePostSchemaType = z.infer<typeof PostSchema>;
+export type InterviewsType = z.infer<typeof InterviewsSchema>;
