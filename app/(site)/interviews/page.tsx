bac0abd4eed4740d916d1bf45a0ad706f8fa8e8f
Hannah-Liao <hanna.liao@outlook.com>
2024-01-01 14:37:12 +0000

    interview page integration

--- app/(site)/interviews/page.tsx
+++ app/(site)/interviews/page.tsx
@@ -6,8 +6,12 @@ import {
   InterviewPostCard,
 } from '@/components/index';
 import { interviewCards } from '@/constants';
+import { getInterviews } from '@/lib/actions/interviews.action';
+import { formatDate } from '@/lib/utils';
 
-export default function Interviews() {
+export default async function Interviews() {
+  const { interviews, totalPages } = await getInterviews(1, 10, 'remote');
+
   return (
     <div className='mt-28 flex h-full w-full flex-col gap-5 lg:flex-row '>
       <section className=' w-full max-w-[210px] max-lg:max-w-full lg:sticky lg:top-28 lg:h-screen'>
@@ -15,17 +19,17 @@ export default function Interviews() {
       </section>
       <div className='flex flex-wrap gap-5'>
         <section className='w-full max-w-[700px] space-y-5 pb-5 max-[1350px]:max-w-full'>
-          {[1, 2, 3].map((post) => (
+          {interviews.map((post) => (
             <InterviewPostCard
-              key={post}
-              image='/assets/images/illustration.png'
-              createdAt='Today, 17 February'
-              name='Valentin Hinov'
-              authorImage='/assets/images/profile.svg'
-              captions='How I Launched and Grew My Startup by 500% During the COVID Crisis'
+              key={post.title}
+              image={post.image}
+              createdAt={formatDate(post.createdAt)}
+              name={post.author.name}
+              authorImage={post.author.image}
+              captions={post.post.slice(0, 80)}
             />
           ))}
-          <Pagination totalPages={20} />
+          <Pagination totalPages={totalPages} />
         </section>
 
         <section className='no-scrollbar w-full space-y-5 max-[1350px]:min-w-full lg:sticky lg:top-28 lg:h-screen lg:max-w-[325px] lg:overflow-y-auto'>
