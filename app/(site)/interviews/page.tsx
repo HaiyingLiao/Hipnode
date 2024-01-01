cdf4b6bf823bc1c6e5138f5b53a0c62fa338b054
Hannah-Liao <hanna.liao@outlook.com>
2024-01-01 13:03:10 +0000

    refactor: fix responsiveness

--- app/(site)/interviews/page.tsx
+++ app/(site)/interviews/page.tsx
@@ -13,7 +13,7 @@ export default function Interviews() {
       <section className=' w-full max-w-[210px] max-lg:max-w-full lg:sticky lg:top-28 lg:h-screen'>
         <Filter />
       </section>
-      <div className=' flex flex-wrap gap-5'>
+      <div className='flex flex-wrap gap-5'>
         <section className='w-full max-w-[700px] space-y-5 pb-5 max-[1350px]:max-w-full'>
           {[1, 2, 3].map((post) => (
             <InterviewPostCard
@@ -27,7 +27,8 @@ export default function Interviews() {
           ))}
           <Pagination totalPages={20} />
         </section>
-        <section className='no-scrollbar w-full min-w-[325px] space-y-5 max-[1350px]:min-w-full lg:sticky lg:top-28 lg:h-screen lg:overflow-y-auto'>
+
+        <section className='no-scrollbar w-full space-y-5 max-[1350px]:min-w-full lg:sticky lg:top-28 lg:h-screen lg:max-w-[325px] lg:overflow-y-auto'>
           <HostMeetupCard cardBtns={interviewCards} />
           <SidePodcasts />
         </section>
