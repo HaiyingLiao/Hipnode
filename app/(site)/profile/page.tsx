2e3259ec13e417a7af1e166e28a721bc0c82f4e4
sujatagunale <sujata@jsmastery.pro>
2024-02-14 00:22:09 +0530

    fix: typescript issue with Generics

--- app/(site)/profile/page.tsx
+++ app/(site)/profile/page.tsx
@@ -10,18 +10,75 @@ import {
 import { cardBtns } from '@/constants';
 import { getProfilePosts } from '@/lib/actions/profile.action';
 import { timeAgo, formatDate } from '@/lib/utils';
-import { SearchParamsProps } from '@/types/searchParamsProps';
 
-export default async function ProfilePage({
-  searchParams,
-}: {
-  searchParams: Pick<SearchParamsProps['searchParams'], 'type' | 'page'>;
-}) {
+interface SearchParamsProps {
+  searchParams: {
+    type: string;
+    page: string;
+  };
+}
+
+interface Interview {
+  id: string;
+  image: string;
+  createdAt: Date;
+  title: string;
+  revenue: number;
+  updates: number;
+  website: string;
+  category: string;
+  authorclerkId: string;
+  author: {
+    name: string;
+    image: string;
+  };
+}
+
+interface Post {
+  id: string;
+  image: string;
+  createdAt: Date;
+  title: string;
+  post: string;
+  category: string;
+  tags: string[];
+  views: number;
+  authorclerkId: string;
+  likes: string[];
+  author: {
+    email: string;
+    name: string;
+  };
+}
+
+/**
+ * ProfilePostsResult<T> is a generic type that takes in a type T and
+ * Returns an object with a data property of type T[] and a totalPages property of type number
+ *
+ * T is a union type of Interview and Post. We used & to create a new type that is a combination of Interview and Post.
+ * If we use | instead of &, the type of data will be Interview[] | Post[] which kinda doesn't work as in JSX we're using
+ * switch case and passing the data to different fields to different components. So typescript will throw an error.
+ *
+ * To avoid that, we use & and specify that ProfilePostResult is a combination of Interview and Post.
+ *
+ * We don't know the type of data that will be returned from the getProfilePosts function. It could be an array of Interview
+ * or Post. So we use a generic type to handle both cases and return the data and totalPages properties.
+ *
+ * Learn more about generic types here: https://www.typescriptlang.org/docs/handbook/2/generics.html
+ * Blog post: https://prismic.io/blog/typescript-generics
+ */
+interface ProfilePostsResult<T> {
+  data: T[];
+  totalPages: number;
+}
+
+export default async function ProfilePage({ searchParams }: SearchParamsProps) {
   const { type, page } = searchParams;
 
-  const postData = await getProfilePosts(type, page);
-  const posts = postData?.data;
-  const totalPages = postData?.totalPages || 1;
+  const { data: posts, totalPages } = (await getProfilePosts(
+    type,
+    page,
+  )) as ProfilePostsResult<Interview & Post>;
 
   return (
     <div className='flex flex-col items-start justify-center gap-6 py-[90px] lg:flex-row lg:py-[100px]'>
